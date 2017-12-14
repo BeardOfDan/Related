b10511ea18036be5a8197bd2b762f6027ae92210
Daniel Roberts <BeardOfDan@gmail.com>
2017-12-13 16:47:00 -0800

    (feat) Add POST paths for /update and /userData

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
 
+// ================
+// === GET PATH ===
+// ================
+
+// This path handles requests from the Entry Point service
 app.get('/related:videoID', (req, res, next) => {
   // Entry Server is looking for related videos to send to client
 
@@ -23,6 +28,56 @@ app.get('/related:videoID', (req, res, next) => {
   res.end(videoID);
 });
 
+
+// ==================
+// === POST PATHS ===
+// ==================
+
+// This path handles requests from the Videos service
+app.post('/update', (req, res, next) => {
+  // get the video from the request body
+  const video = req.body;
+
+  // ensure that only the desired fields are accepted
+  const desiredFields = ['update', '_id', 'title', 'description', 'tags', 'channelId', 'categoryId', 'topicCategories', 'thumbnailURL', 'publishedAt', 'duration'];
+  let desiredFieldsCount = 0;
+  const keys = Object.keys(video);
+  for (let i = 0; i < keys.length; i++) {
+    if (desiredFields.indexOf(keys[i]) === -1) {
+      delete video[keys[i]];
+    } else {
+      desiredFieldsCount++;
+    }
+  }
+
+  // Check for an error with the number of fields received
+  // NOTE: it is possible that there may simply be fewer than normal fields, without an error, follow up on this later with Dustin
+  if (desiredFields !== desiredFields.length) {
+    console.log(`Probable error in the POST path /update\nExpected ${desiredFields.length} fields, but recieved ${desiredFieldsCount} fields instead!`);
+  }
+
+  // update my database with this new video (as a node)
+  // then send a response to signify receipt of the video
+  console.log('add video...');
+  res.end(JSON.stringify(video, undefined, 2));
+
+  // then add edges to the new video
+  console.log('add edges...');
+});
+
+// This path handles requests from the History service
+app.post('/userData', (req, res, next) => {
+  const { data } = req.body;
+
+  // update the edges between the relevent nodes with the order of the data as a sequence
+
+  // The response is the number of videos received
+  // This may prove useful for debugging at some point
+  res.end(`${data.length}`);
+});
+
+
+// path to update video view count
 
 
 
